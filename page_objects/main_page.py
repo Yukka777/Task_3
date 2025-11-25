905ff79fd0a8ae64c3b44d9e277d8ad4fbf9416f
Yukka777 <109177551+Yukka777@users.noreply.github.com>
2025-11-25 12:24:30 +0300

    Update main_page.py

--- page_objects/main_page.py
+++ page_objects/main_page.py
@@ -5,80 +5,80 @@ import allure
 
 class MainPage(BasePage):
     
-    @allure.step('Кликнуть по кнопке перехода в личный кабинет в хэдере')
+    @allure.step('Перейти в личный кабинет через верхнюю панель')
     def click_on_personal_account_in_header(self):
-        """Клик по кнопке личного кабинета в верхней панели навигации"""
+        """Активация элемента навигации для перехода в профиль пользователя"""
         self.click_on_element(MainPageLocators.BUTTON_PERSONAL_ACCOUNT)
 
         
-    @allure.step('Кликнуть по кнопке "Лента заказов" в хэдере')
+    @allure.step('Активировать раздел ленты заказов через верхнюю панель')
     def click_header_feed_button(self):
-        """Клик по кнопке перехода в ленту заказов в хедере"""
+        """Переход к ленте заказов через элемент навигации в шапке"""
         self.click_on_element(MainPageLocators.BUTTON_ORDER_FEED_IN_HEADER)
 
 
-    @allure.step('Переход на страницу конструктора')
+    @allure.step('Вернуться в конструктор бургеров')
     def click_on_button_constructor(self):
-        """Клик по кнопке конструктора для возврата на главную страницу"""
+        """Возврат на основную страницу конструктора через навигацию"""
         self.click_on_element(MainPageLocators.BUTTON_CONSTRUCTOR)
 
 
-    @allure.step('Получение главного заголовка конструктора')
+    @allure.step('Получить заголовок конструктора')
     def get_text_on_title_of_constructor(self):
-        """Получение текста заголовка конструктора для проверки навигации"""
+        """Извлечение текста заголовка для подтверждения отображения конструктора"""
         return self.get_text_on_element(MainPageLocators.CONSTRUCTOR_TITLE)
     
     
-    @allure.step('Кликнуть по кнопке "Войти в аккаунт" на главной')
+    @allure.step('Выполнить вход через главную страницу')
     def click_on_button_login_in_main(self):
-        """Клик по кнопке входа в аккаунт на главной странице"""
+        """Инициация процесса аутентификации с главной страницы"""
         self.click_on_element(MainPageLocators.BUTTON_LOGIN_TO_ORDER)
 
 
-    @allure.step('Кликнуть по ингредиенту')
+    @allure.step('Выбрать компонент бургера')
     def click_on_ingredient(self):
-        """Клик по ингредиенту для открытия модального окна с деталями"""
+        """Открытие детальной информации о компоненте бургера"""
         self.click_on_element(MainPageLocators.BURGER_INGREDIENT)
 
 
-    @allure.step('Проверить отображение окна "Детали ингредиента"')
+    @allure.step('Проверить отображение деталей компонента')
     def check_displaying_of_modal_details(self):
-        """Проверка отображения модального окна с деталями ингредиента"""
+        """Подтверждение отображения окна с характеристиками компонента"""
         return self.check_displaying_of_element(MainPageLocators.MODAL_OF_INGREDIENT)
     
     
-    @allure.step('Проверить, что окно "Детали ингредиента" не отображается')
+    @allure.step('Проверить отсутствие окна деталей компонента')
     def check_not_displaying_of_modal_details(self):
-        """Проверка что модальное окно с деталями ингредиента закрыто"""
+        """Подтверждение закрытия окна с информацией о компоненте"""
         return self.check_not_displaying_of_element(MainPageLocators.MODAL_OF_INGREDIENT)
     
 
-    @allure.step('Закрыть окно "Детали ингредиента"')
+    @allure.step('Закрыть детали компонента"')
     def close_modal(self):
-        """Закрытие модального окна кликом по крестику"""
+        """Закрытие окна с детальной информацией о компоненте"""
         self.click_on_element(MainPageLocators.BUTTON_CLOSE_MODAL)
 
 
-    @allure.step('Добавить ингредиенты')
+    @allure.step('Добавить компонент в заказ')
     def drag_and_drop_ingredient_to_order(self):
-        """Добавление ингредиента в заказ через drag-and-drop"""
+        """Перемещение компонента в область сборки бургера"""
         source_element = self.find_element_with_wait(MainPageLocators.BURGER_INGREDIENT)
         target_element = self.find_element_with_wait(MainPageLocators.BASKET_FOR_INGREDIENTS)
         
-        # Пробуем JavaScript drag-and-drop
+        # Приоритетная попытка через JavaScript
         try:
             self.execute_javascript_drag_and_drop(source_element, target_element)
         except:
-            # Если не сработало, используем стандартный метод
+            # Резервный метод через стандартные действия
             self.drag_and_drop_element(source_element, target_element)
         
-        # Даем время для обновления интерфейса
+        # Ожидание применения изменений в интерфейсе
         self.wait_for_ui_update()
 
 
-    @allure.step('Получить количество ингредиентов')
+    @allure.step('Получить счетчик добавленных компонентов')
     def get_count_of_ingredients(self):
-        """Получение значения счетчика добавленных ингредиентов"""
+        """Получение количества выбранных компонентов из счетчика"""
         try:
             self.wait_visibility_of_element(MainPageLocators.COUNT_OF_INGREDIENTS, timeout=10)
             count_text = self.get_text_on_element(MainPageLocators.COUNT_OF_INGREDIENTS)
@@ -87,35 +87,35 @@ class MainPage(BasePage):
             return "0"
         
         
-    @allure.step('Кликнуть на кнопку создания заказа')
+    @allure.step('Создать заказ')
     def click_on_button_make_order(self):
-        """Клик по кнопке оформления заказа"""
+        """Подтверждение формирования заказа"""
         self.click_on_element(MainPageLocators.BUTTON_MAKE_ORDER)
 
 
-    @allure.step('Проверить отображение окна о создании заказа')
+    @allure.step('Проверить отображение подтверждения заказа')
     def check_displaying_of_confirmation_modal_of_order(self):
-        """Проверка отображения модального окна подтверждения заказа"""
+        """Подтверждение отображения окна с информацией о заказе"""
         return self.check_displaying_of_element(MainPageLocators.ORDER_MODAL)
     
     
-    @allure.step('Получить номер в окне о создании заказа')
+    @allure.step('Получить идентификатор заказа')
     def get_number_of_order_in_modal_confirmation(self):
-        """Получение номера созданного заказа из модального окна"""
+        """Извлечение номера заказа из окна подтверждения"""
         self.wait_visibility_of_element(MainPageLocators.ORDER_NUMBER_CONFIRM, timeout=30)
         self.wait_for_element_to_have_valid_text(MainPageLocators.ORDER_NUMBER_CONFIRM, timeout=30)
         return self.get_text_on_element(MainPageLocators.ORDER_NUMBER_CONFIRM)
     
     
-    @allure.step('Кликнуть на кнопку закрытия окна о создании заказа')
+    @allure.step('Закрыть окно подтверждения заказа')
     def click_on_button_close_confirmation_modal(self):
-        """Закрытие модального окна с номером заказа"""
+        """Закрытие окна с информацией о заказе"""
         self.click_on_element(MainPageLocators.BUTTON_CLOSE_CONFIRMATION)
 
         
-    @allure.step('Выполнить вход в аккаунт')
+    @allure.step('Авторизоваться в системе')
     def login(self, email, password):
-        """Полный процесс авторизации пользователя"""
+        """Выполнение полного процесса аутентификации пользователя"""
         self.click_on_element(MainPageLocators.BUTTON_LOGIN_TO_ORDER)
         self.send_keys_to_input(AccountPageLocators.INPUT_EMAIL, email)
         self.send_keys_to_input(AccountPageLocators.INPUT_PASSWORD, password)
@@ -123,26 +123,26 @@ class MainPage(BasePage):
         self.wait_visibility_of_element(MainPageLocators.BUTTON_MAKE_ORDER, timeout=15)
 
 
-    # НОВЫЕ МЕТОДЫ ДЛЯ ИСПРАВЛЕНИЯ ТЕСТОВ
-    @allure.step('Дождаться обработки заказа')
+    # Дополнительные методы для повышения надёжности тестов
+    @allure.step('Ожидать обработки заказа')
     def wait_for_order_processing(self, timeout=30):
-        """Ожидание завершения обработки заказа и появления номера"""
+        """Ожидание завершения формирования заказа и получения номера"""
         self.wait_visibility_of_element(MainPageLocators.ORDER_NUMBER_CONFIRM, timeout)
 
 
-    @allure.step('Проверить отображение ингредиента')
+    @allure.step('Проверить доступность компонента')
     def check_ingredient_displayed(self):
-        """Проверка отображения ингредиента"""
+        """Подтверждение отображения элемента компонента бургера"""
         return self.check_displaying_of_element(MainPageLocators.BURGER_INGREDIENT)
     
 
-    @allure.step('Проверить отображение корзины для ингредиентов')
+    @allure.step('Проверить отображение области сборки')
     def check_basket_displayed(self):
-        """Проверка отображения корзины для ингредиентов"""
+        """Подтверждение отображения контейнера для компонентов бургера"""
         return self.check_displaying_of_element(MainPageLocators.BASKET_FOR_INGREDIENTS)
     
 
-    @allure.step('Проверить отображение кнопки конструктора')
+    @allure.step('Проверить доступность навигации в конструктор')
     def check_constructor_button_displayed(self):
-        """Проверка отображения кнопки конструктора"""
+        """Подтверждение отображения элемента возврата в конструктор"""
         return self.check_displaying_of_element(MainPageLocators.BUTTON_CONSTRUCTOR)
