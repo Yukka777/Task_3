91f1b9b1229269a72a25fe1d367a3487ebe1346e
Yukka777 <109177551+Yukka777@users.noreply.github.com>
2025-11-25 12:22:31 +0300

    Update feed_page_locators.py

--- locators/feed_page_locators.py
+++ locators/feed_page_locators.py
@@ -2,29 +2,29 @@ from selenium.webdriver.common.by import By
 
 
 class FeedPageLocators:
-    # Раздел заказов
+    # Блок с перечнем заказов
     SECTION_ORDERS_LIST = (By.XPATH, '//ul[contains(@class, "OrderFeed_list")]')
 
-    # Заголовок ленты заказов
+    # Название раздела ленты заказов
     TITLE_OF_ORDERS_FEED = (By.XPATH, '//h1[text()="Лента заказов"]')
 
-    # Всплывающее окно с деталями заказа
+    # Модальное окно деталей заказа
     MODAL_ORDER = (By.XPATH, '//section[contains(@class, "Modal_modal")]')
 
-    # Заголовок всплывающего окна с деталями заказа
+    # Заголовок модального окна с информацией о заказе
     TITLE_OF_MODAL_ORDER = (By.XPATH, '//section[contains(@class, "Modal_modal")]//h2')
 
-    # Счетчик заказов "Выполнено за все время"
+    # Индикатор общего количества выполненных заказов
     ORDER_COUNTER_BY_ALL_TIME = (By.XPATH, "//p[contains(text(),'Выполнено за все время')]/following-sibling::p[contains(@class, 'OrderFeed_number')]")
 
-    # Счетчик заказов "Выполнено за сегодня"
+    # Индикатор количества заказов за текущие сутки
     ORDER_COUNTER_BY_TODAY = (By.XPATH, "//p[contains(text(),'Выполнено за сегодня')]/following-sibling::p[contains(@class, 'OrderFeed_number')]")
 
-    # Заказ в разделе "В работе"
+    # Элементы заказов в статусе выполнения
     ORDER_IN_PROGRESS = (By.XPATH, ".//ul[contains(@class, 'OrderFeed_orderListReady')]//li") 
     
-    # Один заказ в истории
+    # Первая позиция в истории заказов
     ORDER_FIRST_IN_HISTORY = (By.XPATH, ".//li[contains(@class, 'OrderHistory_listItem')][1]")
     
-    # Все заказы в истории
+    # Все элементы истории заказов
     ALL_ORDERS_HISTORY = (By.XPATH, './/p[contains(@class, "text_type_digits-default")]')
