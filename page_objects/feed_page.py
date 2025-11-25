292232400c20330a9d6bda08eea313d29cf2de85
Yukka777 <109177551+Yukka777@users.noreply.github.com>
2025-11-25 12:24:07 +0300

    Update feed_page.py

--- page_objects/feed_page.py
+++ page_objects/feed_page.py
@@ -4,24 +4,24 @@ import allure
 
 class FeedPage(BasePage):
     
-    @allure.step('Получить текст заголовка раздела заказов')
+    @allure.step('Получить заголовок ленты заказов')
     def get_text_on_title_of_orders_list(self):
-        """Получение заголовка страницы ленты заказов для проверки навигации"""
+        """Извлечение текста заголовка для подтверждения отображения страницы"""
         return self.get_text_on_element(FeedPageLocators.TITLE_OF_ORDERS_FEED)
 
-    @allure.step('Получить количество заказов, выполненных за все время')
+    @allure.step('Получить общее количество выполненных заказов')
     def get_quantity_of_orders(self):
-        """Получение значения счетчика заказов за все время"""
+        """Получение значения счетчика выполненных заказов за весь период"""
         self.find_element_with_wait(FeedPageLocators.ORDER_COUNTER_BY_ALL_TIME)
         return self.get_text_on_element(FeedPageLocators.ORDER_COUNTER_BY_ALL_TIME)
 
-    @allure.step('Получить количество заказов, выполненных за сегодня')
+    @allure.step('Получить количество заказов за текущий день')
     def get_daily_quantity_of_orders(self):
-        """Получение значения счетчика заказов за текущий день"""
+        """ППолучение значения счетчика заказов выполненных сегодня"""
         self.find_element_with_wait(FeedPageLocators.ORDER_COUNTER_BY_TODAY)
         return self.get_text_on_element(FeedPageLocators.ORDER_COUNTER_BY_TODAY)
 
-    @allure.step('Получить номер последнего заказа в разделе "В работе"')
+    @allure.step('Получить номер заказа в разделе выполняемых"')
     def get_order_number_in_feed_progress_section(self):
-        """Получение номера заказа из раздела 'В работе' в ленте заказов"""
+        """Извлечение номера заказа из секции текущих выполнений"""
         return self.get_text_on_element(FeedPageLocators.ORDER_IN_PROGRESS)
