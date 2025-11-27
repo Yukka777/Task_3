96941dd2c91be1aa74ea8ac5af048214ad57c0de
Yukka777 <109177551+Yukka777@users.noreply.github.com>
2025-11-28 00:39:01 +0300

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,5 +1,6 @@
 class Urls:
     BASE_URL = 'https://stellarburgers.education-services.ru'
-    USER_REGISTER = f'{BASE_URL}/api/auth/register'    # Используется в create_new_user_and_delete
-    USER_DELETE = f'{BASE_URL}/api/auth/user'          # Используется в create_new_user_and_delete  
-    ORDER_CREATE = f'{BASE_URL}/api/orders'            # Используется (потенциально) для заказов
+    USER_REGISTER = f'{BASE_URL}/api/auth/register'
+    USER_DELETE = f'{BASE_URL}/api/auth/user'
+    ORDER_CREATE = f'{BASE_URL}/api/orders'
+    RESET_PASSWORD = f'{BASE_URL}/reset-password'
